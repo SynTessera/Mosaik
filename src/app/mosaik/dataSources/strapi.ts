cf4b2f33e33c6638445d790a426221ed1a2b1878
Moritz Roessler <moritz.roessler@digitas.com>
2025-07-12 05:01:02 +0200

    fix: add ISR with 30s revalidate

--- src/app/mosaik/dataSources/strapi.ts
+++ src/app/mosaik/dataSources/strapi.ts
@@ -1,33 +1,38 @@
 export const fetchBlogPosts = async () => {
-    const url = `${process.env.STRAPI_API}/blog-posts?[pagination][pageSize]=1000&populate=*`;
-    const prom = await fetch(url, {
-        headers: {
-            "Authorization": process.env.STRAPI_TOKEN || ''
-        }
-    });
-    const json = await prom.json();
-    return json.data;
-}
+  const url = `${process.env.STRAPI_API}/blog-posts?[pagination][pageSize]=1000&populate=*`;
+  const prom = await fetch(url, {
+    headers: {
+      Authorization: process.env.STRAPI_TOKEN || "",
+    },
+    next: {
+      revalidate: 30,
+    },
+  });
+  const json = await prom.json();
+  return json.data;
+};
 
 export const fetchBlogPost = async (id: string) => {
-    const url = `${process.env.STRAPI_API}/blog-posts/${id}?populate=*`;
-    const prom = await fetch(url, {
-        headers: {
-            "Authorization": process.env.STRAPI_TOKEN || ''
-        }
-    });
-    const json = await prom.json();
-    return json.data;
-}
-
+  const url = `${process.env.STRAPI_API}/blog-posts/${id}?populate=*`;
+  const prom = await fetch(url, {
+    headers: {
+      Authorization: process.env.STRAPI_TOKEN || "",
+    },
+    next: {
+      revalidate: 30,
+    },
+  });
+  const json = await prom.json();
+  return json.data;
+};
 
 export const fetchSections = async () => {
-    const url = `${process.env.STRAPI_API}/sections?[pagination][pageSize]=1000&populate=*`;
-    const prom = await fetch(url, {
-        headers: {
-            "Authorization": process.env.STRAPI_TOKEN || ''
-        }
-    });
-    const json = await prom.json();
-    return json.data;
-}
+  const url = `${process.env.STRAPI_API}/sections?[pagination][pageSize]=1000&populate=*`;
+  const prom = await fetch(url, {
+    headers: {
+      Authorization: process.env.STRAPI_TOKEN || "",
+    },
+  });
+  const json = await prom.json();
+  return json.data;
+};
