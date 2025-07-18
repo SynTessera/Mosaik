072ad5d46087f5b1abd47f298aef61e9a4364c4c
Moritz Roessler <moritz.roessler@digitas.com>
2025-07-18 23:10:18 +0200

    wip: experimental hydration

--- src/blocks/hybrid/NavigationLinkClient.tsx
+++ src/blocks/hybrid/NavigationLinkClient.tsx
@@ -3,11 +3,22 @@
 import { useRouter } from "next/navigation";
 import { useAppState } from "@/context/StateContext";
 import { useThemedComponent } from "@/lib/hooks/useThemedComponent";
+import { useEffect, useState } from "react";
 
-export const NavigationLinkClient = (props: any) => {
+export const NavigationLinkClient = ({ Component, ...props }: any) => {
   const NavLink = useThemedComponent("NavigationLink");
   const { state } = useAppState();
   const router = useRouter();
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  if (!hydrated || !NavLink) {
+    // Before hydration, render the server-rendered static fallback
+    return Component;
+  }
   return (
     <NavLink
       {...props}
