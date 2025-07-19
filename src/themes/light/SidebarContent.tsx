2d4401128a87f19efb45c5385907e48a590f1ca6
Moritz Roessler <moritz.roessler@digitas.com>
2025-07-20 01:00:58 +0200

    fix: preven layout shift on scrollable hover

--- src/themes/light/SidebarContent.tsx
+++ src/themes/light/SidebarContent.tsx
@@ -17,7 +17,7 @@ export const SidebarContent = ({
     className={clsx(
       "SidebarContent",
       classes.SidebarContent,
-      "min-w-[var(--sidebar-min-width)] flex-1 h-full scrollable",
+      "min-w-[var(--sidebar-min-width)] flex-1 h-full scrollable pr-[10px] hover:mr-0 hover:pr-[0px]",
       {
         hidden: state?.sidebar?.expanded === 0,
         block: state?.sidebar?.expanded > 0,
