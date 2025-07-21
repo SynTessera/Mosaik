6903cffbf184c011b3fbeee095ff04630802dc64
Moritz Roessler <moritz.roessler@digitas.com>
2025-07-21 03:51:56 +0200

    fix: refine blog post layout

--- src/themes/light/BlogPost.tsx
+++ src/themes/light/BlogPost.tsx
@@ -7,18 +7,35 @@ export const BlogPost = ({ data = [], className }: any) => {
   const { title, coverImage, content, documentId, slug } = data?.[0];
 
   return (
-    <article className="flex flex-col lg:flex-row gap-4 bg-gray-400/40 max-h-full w-full overflow-y-auto overflow-x-hidden">
-      <img
-        src={coverImageLink(coverImage?.url)}
-        className={clsx(
-          "shrink w-1/4 object-contain h-fit sticky top-4 my-4",
-          className
-        )}
-      />
-      <div className="h-full  mx-auto flex-1 grow py-4">
+    <article className="flex flex-col lg:flex-row gap-4 bg-white/10 max-h-full w-full overflow-y-auto overflow-x-hidden">
+      <div className="h-full mx-auto flex-1 grow pb-4 relative">
         <h1>
           <Link href={`/mosaik/blog/${slug}-${documentId}`}>{title}</Link>
         </h1>
+        <div className="relative overflow-hidden flex justify-center">
+                    <img
+            src={coverImageLink(coverImage?.url)}
+            className={clsx(
+              "w-full sm:w-2/3 md:w-1/2  mx-auto",
+              className
+            )}
+            alt={"Blog Post Cover Image"}
+          />
+          <img
+            src={coverImageLink(coverImage?.url)}
+            className={clsx("absolute w-full   h-fit -top-1/4  z-10 blur-md", className)}
+            alt={"Blog Post Cover Image"}
+          />
+          <img
+            src={coverImageLink(coverImage?.url)}
+            className={clsx(
+              "absolute w-full sm:w-2/3 md:w-1/2  mx-auto z-30",
+              className
+            )}
+            alt={"Blog Post Cover Image"}
+          />
+        </div>
+
         <MarkdownContainer>{content}</MarkdownContainer>
       </div>
     </article>
