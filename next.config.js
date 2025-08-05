ce3bab9de646a5b0f0cfd8f437db5d041b9ea1b4
Moritz Roessler <moritz.roessler@digitas.com>
2025-08-05 23:27:30 +0200

    fix: whitelist strapi image domain

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
         pathname: "/uploads/**",
       },
       {
+        protocol: "https",
+        hostname: "strapidigitasjavascriptmoe-production.up.railway.app",
+        // Optional: Restrict to specific paths
+        pathname: "/uploads/**",
+      },
+      {
         protocol: "http",
         hostname: "127.0.0.1",
         // Optional: Restrict to specific paths
@@ -19,29 +25,29 @@ const nextConfig = {
   experimental: {
     // Force Webpack instead of Turbopack
     turbo: {
-      
+
     },
   },
-webpack(config, options) {
-  // Remove existing svg handling
-  config.module.rules.forEach((rule) => {
-    if (rule?.test?.toString().includes('svg')) {
-      rule.exclude = /\.svg$/i;
-    }
-  });
+  webpack(config, options) {
+    // Remove existing svg handling
+    config.module.rules.forEach((rule) => {
+      if (rule?.test?.toString().includes('svg')) {
+        rule.exclude = /\.svg$/i;
+      }
+    });
 
-  // Add SVGR loader for svg imports in JS/TS files
-  config.module.rules.push({
-    test: /\.svg$/i,
-    use: [
-      {
-        loader: require.resolve('@svgr/webpack'),
-      },
-    ],
-  });
+    // Add SVGR loader for svg imports in JS/TS files
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [
+        {
+          loader: require.resolve('@svgr/webpack'),
+        },
+      ],
+    });
 
-  return config;
-}
+    return config;
+  }
 };
 
 module.exports = nextConfig;
