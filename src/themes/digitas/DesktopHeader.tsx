db36240aabe7648942738bdca7c75d0a31d18d06
Moritz Roessler <moritz.roessler@digitas.com>
2025-08-06 13:28:36 +0200

    fix: animate header transition

--- src/themes/digitas/DesktopHeader.tsx
+++ src/themes/digitas/DesktopHeader.tsx
@@ -9,22 +9,25 @@ export const DesktopHeader = ({
   link,
   collapsed,
 }: any) => {
-  return <div
-    className={clsx(
-      "w-full h-min flex items-center grow flex-col z-10",
-      className
-      // { "h-[80px] overflow-hidden": collapsed }
-    )}
-  >
-    {!collapsed && (
-      <HeaderSection className="uppercase">
-        <div>{headline}</div>
-        <div>{subline}</div>
+  return (
+    <div
+      className={clsx(
+        "w-full  transition-all duration-200 flex items-center grow flex-col z-10",
+        className,
+        { "h-[244px] mt-0": !collapsed, "-mt-[122px]": collapsed }
+        // { "h-[80px] overflow-hidden": collapsed }
+      )}
+    >
+      {1 && (
+        <HeaderSection className="uppercase">
+          <div>{headline}</div>
+          <div>{subline}</div>
+        </HeaderSection>
+      )}
+      <HeaderSection className="flex justify-between items-center">
+        <DigitasLogo />
+        <InternalLink href={link?.href}>{link.linkText}</InternalLink>
       </HeaderSection>
-    )}
-    <HeaderSection className="flex justify-between items-center">
-      <DigitasLogo />
-      <InternalLink href={link?.href}>{link.linkText}</InternalLink>
-    </HeaderSection>
-  </div>
+    </div>
+  );
 };
